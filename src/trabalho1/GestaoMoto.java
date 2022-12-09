89cf88acc7ed0b0927709aa4c3d041b6cb1e0a99
NalbertC <nalberthcastro1510@gmail.com>
2022-12-09 17:51:41 -0300

    gerenciamento finalizado

--- src/trabalho1/GestaoMoto.java
+++ src/trabalho1/GestaoMoto.java
@@ -9,32 +9,35 @@ public class GestaoMoto {
         Scanner sc = new Scanner(System.in);
 
         do {
-            System.out.println("\n======= Gestão de Motos =======\n\n1 - Cadastrar moto;\n2 - Visualizar motos;\n3 - Utualizar dados de uma moto;\n4 - Deletar uma moto do Gerenciador;\n0 - Fechar o sistema;\nEscolha uma ação: ");
+            System.out.println("\n======= Gestão de Motos =======\n\n1 - Cadastrar moto;\n2 - Visualizar motos;\n3 - Visualizar uma moto;\n4 - Utualizar dados de uma moto;\n5 - Deletar uma moto do Gerenciador;\n0 - Fechar o sistema;\nEscolha uma ação: ");
             escolha = sc.nextInt();
 
             switch (escolha) {
-                case 0:
+                case 0 -> {
                     System.out.println("\nFechando o sistema......");
                     System.exit(0);
-                    break;
-                case 1:
-                    System.out.println("\nCadastrar Moto no Sistema:");
-
+                }
+                case 1 -> {
+                    System.out.println("\nCadastrar Moto no Sistema: ");
                     Gerenciamento.cadastrarMoto();
-                    break;
-                case 2:
-                    System.out.println("\nListar todas as motos cadastradas:");
-                    System.out.println(Gerenciamento.consultarMoto());
-                   
-                    break;
-                case 3:
-                    System.out.println("\nAtualizar dados de uma moto:");
-                    break;
-                case 4:
-                    System.out.println("\nExcluir cadastro de uma moto:");
-                    break;
-                default:
-                    System.out.println("\nOpção inválida escolha outra opção!");
+                }
+                case 2 -> {
+                    System.out.println("\nListar todas as motos cadastradas: ");
+                    System.out.println(Gerenciamento.listarMotos());
+                }
+                case 3 -> {
+                    System.out.println("\nVisualizar informações de uma moto: ");
+                    Gerenciamento.consultarMoto();
+                }
+                case 4 -> {
+                    System.out.println("\nAtualizar dados de uma moto: ");
+                    Gerenciamento.atualizarMoto();
+                }
+                case 5 -> {
+                    System.out.println("\nExcluir cadastro de uma moto: ");
+                    Gerenciamento.exluirMoto();
+                }
+                default -> System.out.println("\nOpção inválida escolha outra opção!");
             }
 
         } while (escolha != 0);
